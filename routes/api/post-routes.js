c46ca6797b3d8077c8066cded3961c9dc40e1307
Sterling Peck <peck.sterling@gmail.com>
2022-06-04 09:11:40 -0600

    posts

---
+++ routes/api/post-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const { Post, User } = require("../../models");
+
+// get all users
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+Post.findAll({
+  attributes: ["id", "post_url", "title", "created_at"],
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const userRoutes = require("./api/user-routes");
+const postRoutes = require("./api/post-routes");
 
 router.use("/api", apiRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
